d55b6b68502845b092469dc986eb2559764ba78a
Xsarz <88231084+xXxCLOTIxXx@users.noreply.github.com>
2024-06-15 22:44:24 +0300

    Delete amino directory

--- amino/objects/argObjects/message_type.py
+++
@@ -1,5 +0,0 @@
-class MessageTypes:
-	Text: int = 0
-	Strike: int = 1
-	Voice: int = 2
-	Sticker: int = 3
--- amino/objects/argObjects/sorting.py
+++
@@ -1,6 +0,0 @@
-class Sorting:
-    Newest: str = "newest"
-    Oldest: str = "oldest"
-    Top: str = "vote"
-
-    all: list = [Newest, Oldest, Top]
